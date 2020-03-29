dff9346d106cf1dbe929bc1cde0e286ce3bec2c0
Evg Pn <ye.ponomarenko@gmail.com>
2020-03-29 19:25:22 +0300

    feat(project) add ScriptableObject scene
    Scene has two cubes. Their colors and names are set using ScriptableObject

---
+++ EducationPart3/Assets/Scripts/ScriptableObject/PlayersManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _players = null;    
+    [SerializeField]
+    private ScriptableObjectClass _playersData = null;
+
+    private void Start()
+    {        
+        SetPlayersSetting();
+    }
+
+    private void SetPlayersSetting()
+    {
+        for (int i = 0; i < _players.Length; i++)
+        {
+            _players[i].name = _playersData.Players[i].Name;
+            _players[i].GetComponent<MeshRenderer>().material.color = _playersData.Players[i].color;
+        }
+    }
+}
---
+++ EducationPart3/Assets/Scripts/ScriptableObject/ScriptableObjectClass.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ScriptableObjectClass", order = 1)]
+public class ScriptableObjectClass : ScriptableObject
+{
+    public string AppName;
+    public string AppVersion;
+    public List<Player> Players;
+}
+
+[Serializable]
+public class Player
+{
+    public string Name;
+    public Color color;
+}
