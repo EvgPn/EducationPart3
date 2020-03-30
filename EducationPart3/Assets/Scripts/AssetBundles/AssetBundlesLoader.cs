d37760062c5f27b861ae69f26b6d89f8366bf635
Evg Pn <ye.ponomarenko@gmail.com>
2020-03-30 11:11:26 +0300

    feat(project) add AssetBundle scene
    Scene has example of AssetBundle pack and unpack

---
+++ EducationPart3/Assets/Scripts/AssetBundles/AssetBundlesLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundlesLoader : MonoBehaviour
+{
+    private AssetBundle _loadedAssetBundle;
+
+    private void Start()
+    {
+        LoadAssetBundle();
+    }
+
+    private void LoadAssetBundle()
+    {
+        _loadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/katana");
+        if(_loadedAssetBundle != null)
+        {
+            Debug.Log("AssetBundle succesfully loaded");
+        }
+    }
+}
---
+++ EducationPart3/Assets/Scripts/AssetBundles/Editor/AssetBundlesBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundlesBuilder
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
